8c8ad8595ba3c4440c0fb7904863e78fd0c7a739
Pouria Chalangari <pouria.chalangari@gmail.com>
2020-11-08 00:15:25 -0500

    Add database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js"); //module defined in date.js
 const app = express();
 const port = 4000;
@@ -21,17 +22,55 @@ app.listen(port, () => {
 
 //#########################################################
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+
+
+//#########################################################
+
 // / route
 app.get("/", (req, res) => {
-const currentDay = date.getDate();
-  res.render("list", {
-    listTitle: currentDay,
-    newItems: newListItems
+  const currentDay = date.getDate();
+
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully inserted default items.");
+          }
+      });
+    } else {
+      res.render("list", {listTitle: currentDay, newItems: foundItems})
+    }
   });
 });
 
+
 // /work route
-app.get("/work", (req,res)=>{
+app.get("/work", (req, res) => {
   res.render("list", {
     listTitle: "Work List",
     newItems: workItems
@@ -39,7 +78,7 @@ app.get("/work", (req,res)=>{
 });
 
 // /about route
-app.get("/about", (req,res)=>{
+app.get("/about", (req, res) => {
   res.render("about");
 });
 
@@ -48,14 +87,16 @@ app.get("/about", (req,res)=>{
 app.post("/", (req, res) => {
   const currentPage = req.body.list;
   const data = req.body.inputData;
-  if (currentPage === "Work List") {
-    workItems.push(data);
-    res.redirect("/work");
-  }
-  else {
-    newListItems.push(data);
-    res.redirect("/");
-  }
+  const newEnteredItem = new Item({
+    name: data
+  });
+  newEnteredItem.save();
+  res.redirect("/");
+
+});
+
+app.post("/delete", function(req, res){
+  console.log(req.body);
 });
 
 //#########################################################
